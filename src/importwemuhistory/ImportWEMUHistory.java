15a03c40c017631e7d2738df0f86db5b46a6ba29
Elkip <Mitchiscool51@Gmail.com>
2019-04-04 10:20:27 -0400

    Update
    The following Apache libraries are required to run:
    PDFBox2.0.14, FontBox2.0.14, commons-logging-1.2

--- src/importwemuhistory/ImportWEMUHistory.java
+++ src/importwemuhistory/ImportWEMUHistory.java
@@ -1,24 +1,36 @@
-
 package importwemuhistory;
 
-import java.io.File;
-import java.io.IOException;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-
-
+import java.io.*;
+import org.apache.pdfbox.pdmodel.*;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.util.*;
 
 public class ImportWEMUHistory {
 
-    public static void main(String[] args) throws IOException {
-
-        //Load Document
+    public static void main(String[] args) {
         File file = new File("/home/elkip/NetBeansProjects/ImportWEMUHistory/mozilla.pdf");
-        PDDocument document = PDDocument.load(file);
         
-        System.out.println("PDF Loaded");
         
-        document.close();
+        ReadPDF(file);
+    }
+    
+    private static void ReadPDF(File f) {
+
+        try {
+            PDDocument pdDoc = PDDocument.load(f);
+            PDFTextStripper stripper = new PDFTextStripper();
+            System.out.println("PDF Loaded");
+            
+            //String builder to store extracted text
+            StringBuilder sb = new StringBuilder();
+            
+            sb.append(stripper.getText(pdDoc));
+            
+            System.out.println(sb);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
 }
