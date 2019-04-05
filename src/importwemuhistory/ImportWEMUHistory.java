eef9b2ba6f37c3af6753f070b7cd48ec918d986a
Elkip <Mitchiscool51@Gmail.com>
2019-04-04 21:40:13 -0400

    Update: Print Doc char by char
    Next: Identify text lines labels.

--- src/importwemuhistory/ImportWEMUHistory.java
+++ src/importwemuhistory/ImportWEMUHistory.java
@@ -17,6 +17,7 @@ public class ImportWEMUHistory {
     private static void ReadPDF(File f) {
 
         try {
+            //create a pdf stripper
             PDDocument pdDoc = PDDocument.load(f);
             PDFTextStripper stripper = new PDFTextStripper();
             System.out.println("PDF Loaded");
@@ -26,7 +27,18 @@ public class ImportWEMUHistory {
             
             sb.append(stripper.getText(pdDoc));
             
-            System.out.println(sb);
+            int offset = 0;
+            //until the end of the document
+            do{
+                //read in line
+                char[] Line = null;
+                int i_end = sb.indexOf("\n",offset);
+                Line = new char[i_end - offset];
+                sb.getChars(offset, i_end, Line, 0);
+                System.out.println(new String(Line));
+                offset = i_end+1;
+
+            } while(offset < sb.length());
         }
         catch (IOException e) {
             e.printStackTrace();
